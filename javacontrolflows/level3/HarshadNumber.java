afd25bc7e34159d0caf278eb245fd45089b0d24e
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:56:32 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level3/HarshadNumber.java
+++ javacontrolflows/level3/HarshadNumber.java
@@ -9,28 +9,43 @@ Inside the loop, add each digit of the number to sum.
 Check if the number is perfectly divisible by the sum.
 If the number is divisible by the sum, print Harshad Number. Otherwise, print Not a Harshad Number.
  */
-
-
 import java.util.Scanner;
 
 public class HarshadNumber {
+
     public static void main(String[] args) {
+
+        // Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
+
+        // Reading the number to be checked
         int number = sc.nextInt();
 
+        // Storing the original number for later comparison
         int originalNumber = number;
+
+        // Variable to store the sum of digits
         int sum = 0;
 
+        // Loop to calculate the sum of digits of the number
         while (number != 0) {
+
+            // Adding the last digit to the sum
             sum = sum + (number % 10);
+
+            // Removing the last digit from the number
             number = number / 10;
         }
 
+        // Checking if the number is divisible by the sum of its digits
         if (originalNumber % sum == 0) {
             System.out.println("Harshad Number");
-        } else {
+        }
+        else {
             System.out.println("Not a Harshad Number");
         }
+
+        // Closing the scanner to release resources
         sc.close();
     }
 }
