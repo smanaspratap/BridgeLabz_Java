145e40d19774956a25236be2a8866400084448a0
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:34:51 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level1/NumberCheck.java
+++ javacontrolflows/level1/NumberCheck.java
@@ -8,17 +8,30 @@
 import java.util.Scanner;
 
 public class NumberCheck {
+
     public static void main(String[] args) {
+
+        // Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
+
+        // Reading the number from the user
         int number = sc.nextInt();
 
-        if (number > 0)
+        // Checking if the number is positive
+        if (number > 0) {
             System.out.println("Positive");
-        else if (number < 0)
+        }
+        // Checking if the number is negative
+        else if (number < 0) {
             System.out.println("Negative");
-        else
+        }
+        // If the number is zero
+        else {
             System.out.println("Zero");
+        }
+
+        // Closing the scanner to release resources
         sc.close();
     }
-    
 }
+
