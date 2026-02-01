be3840f351e0075c39aa7195c832a3a990ec0a0a
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:47:12 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level2/Leapyear.java
+++ javacontrolflows/level2/Leapyear.java
@@ -5,34 +5,43 @@ Further, the Leap Year is a Year divisible by 4 and not 100 unless it is divisib
 Write code having multiple if else statements based on conditions provided above and a second part having only one if statement and multiple logical 
  */
 
-
-
-
 import java.util.Scanner;
 
 public class Leapyr {
+
     public static void main(String[] args) {
+
+        // Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
 
+        // Prompting the user to enter a year
         System.out.print("Enter the year: ");
         int year = sc.nextInt();
 
+        // Checking if the year is valid according to the Gregorian calendar
         if (year < 1582) {
+
+            // Gregorian calendar started from the year 1582
             System.out.println("Year must be 1582 or later (Gregorian Calendar).");
-        } 
+        }
+        // If the year is divisible by 400, it is a leap year
         else if (year % 400 == 0) {
             System.out.println("Year is a Leap Year");
-        } 
+        }
+        // If the year is divisible by 100 but not by 400, it is NOT a leap year
         else if (year % 100 == 0) {
             System.out.println("Year is not a Leap Year");
-        } 
+        }
+        // If the year is divisible by 4, it is a leap year
         else if (year % 4 == 0) {
             System.out.println("Year is a Leap Year");
-        } 
+        }
+        // All other years are not leap years
         else {
             System.out.println("Year is not a Leap Year");
         }
 
+        // Closing the scanner to release resources
         sc.close();
     }
 }
