2bf063c4b37b3eea1cbfaae06d0d2300ad8913b2
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:35:33 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level1/OddEven.java
+++ javacontrolflows/level1/OddEven.java
@@ -8,18 +8,33 @@ In each iteration of the loop, print the number is odd or even number
 import java.util.Scanner;
 
 public class OddEven {
+
     public static void main(String[] args) {
+
+        // Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
+
+        // Reading the number up to which odd/even check is required
         int number = sc.nextInt();
 
+        // Checking if the number is valid (natural number)
         if (number >= 1) {
+
+            // Loop from 1 to the given number
             for (int i = 1; i <= number; i++) {
-                if (i % 2 == 0)
+
+                // Checking if the current number is even
+                if (i % 2 == 0) {
                     System.out.println(i + " is Even");
-                else
+                }
+                // If the current number is odd
+                else {
                     System.out.println(i + " is Odd");
+                }
             }
         }
+
+        // Closing the scanner to free resources
         sc.close();
     }
 }
