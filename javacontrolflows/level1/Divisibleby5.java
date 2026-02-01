ea704c466c4647de68c8409a8fa46b554bfad520
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:38:28 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level1/Divisibleby5.java
+++ javacontrolflows/level1/Divisibleby5.java
@@ -4,15 +4,29 @@
 
 
 import java.util.Scanner;
-public class divisibleby5 {
+
+public class Divisibleby5 {
+
     public static void main(String[] args) {
+
+        // Scanner object to read input from the user
         Scanner inp = new Scanner(System.in);
+
+        // Reading the number from the user
         int n = inp.nextInt();
-        if(n%5 == 0){
+
+        // Checking if the number is divisible by 5
+        if (n % 5 == 0) {
+
+            // If remainder is 0, the number is divisible by 5
             System.out.println("Number is Div by 5");
-        }else{
+        }
+        // If remainder is not 0
+        else {
             System.out.println("Not Div");
         }
+
+        // Closing the scanner to release resources
         inp.close();
     }
 }
