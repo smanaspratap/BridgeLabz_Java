027f7bda75b43f9b94d5ae68a130147e4512d17d
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 01:07:18 +0530

    Update and rename athlete.java to Athleterunning.java [Manas Pratap Singh]: Solved all the Java Methods Problems Level 1,2&3

--- javamethods/level1/Athleterunning.java
+++ javamethods/level1/Athleterunning.java
@@ -5,19 +5,38 @@ The perimeter of a triangle is the addition of all sides and rounds is distance/
 Write a Method to compute the number of rounds user needs to do to complete 5km run
  */
 import java.util.Scanner;
+
 public class athlete {
-   static double numberOfRounds(double a, double b, double c) {
+
+    // This static method calculates the number of rounds an athlete must run
+    // a, b, c represent the three sides of a triangular track
+    static double numberOfRounds(double a, double b, double c) {
+
+        // Calculate the perimeter of the triangular track
         double perimeter = a + b + c;
+
+        // Total distance to be covered is 5000 meters
+        // Number of rounds = total distance / perimeter of the track
         return 5000 / perimeter;
     }
 
     public static void main(String[] args) {
+
+        // Create a Scanner object to take input from the user
         Scanner sc = new Scanner(System.in);
+
+        // Read the three side lengths of the triangular track
         double a = sc.nextDouble();
         double b = sc.nextDouble();
         double c = sc.nextDouble();
+
+        // Call the method to calculate number of rounds
         double rounds = numberOfRounds(a, b, c);
+
+        // Print the number of rounds required
         System.out.println(rounds);
+
+        // Close the Scanner to free resources
         sc.close();
-    } 
+    }
 }
