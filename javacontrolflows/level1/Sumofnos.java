22c71575800e90c20f84bbd7ce8222fab8c1a773
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:43:37 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level1/Sumofnos.java
+++ javacontrolflows/level1/Sumofnos.java
@@ -9,15 +9,29 @@ The loop will continue till the user enters zero and outside the loop display th
 import java.util.Scanner;
 
 public class sumofnos {
+
     public static void main(String[] args) {
+
+        // Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
+
+        // Variable to store the total sum of entered numbers
         double total = 0.0;
+
+        // Variable to store each input value
         double value;
 
+        // Loop continues until the user enters 0
         while ((value = sc.nextDouble()) != 0) {
+
+            // Adding the entered value to the total
             total += value;
         }
+
+        // Displaying the final sum
         System.out.println("Total = " + total);
+
+        // Closing the scanner to release resources
         sc.close();
     }
 }
