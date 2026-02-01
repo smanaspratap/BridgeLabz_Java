adbd159f95e3c8683358fbebd2450563ab61a836
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:36:47 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level1/SumBreak.java
+++ javacontrolflows/level1/SumBreak.java
@@ -3,22 +3,38 @@ Hint =>
 Use infinite while loop as in while (true)
 Take the user entry and check if the user entered 0 or a negative number to break the loop using break;
  */
-
-
 import java.util.Scanner;
 
 public class SumBreak {
+
     public static void main(String[] args) {
+
+        // Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
+
+        // Variable to store the total sum of entered values
         double total = 0.0;
 
+        // Infinite loop to keep taking input
         while (true) {
+
+            // Reading a value from the user
             double value = sc.nextDouble();
-            if (value <= 0)
+
+            // If value is zero or negative, exit the loop
+            if (value <= 0) {
                 break;
+            }
+
+            // Adding the value to the total
             total += value;
         }
+
+        // Displaying the final sum
         System.out.println("Total = " + total);
+
+        // Closing the scanner to release resources
         sc.close();
     }
 }
+
