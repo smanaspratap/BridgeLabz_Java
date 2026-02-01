4860e25bb5f23a0e1e32a568d6c919e9db193f3f
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:32:56 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level1/Largestnos.java
+++ javacontrolflows/level1/Largestnos.java
@@ -10,26 +10,39 @@
 import java.util.Scanner;
 
 public class Largestnos {
+
     public static void main(String[] args) {
+
+        // Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
 
+        // Taking the first number as input
         System.out.print("Enter number1: ");
         int number1 = sc.nextInt();
 
+        // Taking the second number as input
         System.out.print("Enter number2: ");
         int number2 = sc.nextInt();
 
+        // Taking the third number as input
         System.out.print("Enter number3: ");
         int number3 = sc.nextInt();
 
-        boolean isFirstLargest  = (number1 > number2) && (number1 > number3);
-        boolean isSecondLargest = (number2 > number1) && (number2 > number3);
-        boolean isThirdLargest  = (number3 > number1) && (number3 > number2);
+        // Checking if the first number is the largest
+        boolean isFirstLargest = (number1 > number2) && (number1 > number3);
 
+        // Checking if the second number is the largest
+        boolean isSecondLargest = (number2 > number1) && (number2 > number3);
+
+        // Checking if the third number is the largest
+        boolean isThirdLargest = (number3 > number1) && (number3 > number2);
+
+        // Displaying the results
         System.out.println("Is the first number the largest? " + isFirstLargest);
         System.out.println("Is the second number the largest? " + isSecondLargest);
         System.out.println("Is the third number the largest? " + isThirdLargest);
 
+        // Closing the scanner to release resources
         sc.close();
     }
 }
