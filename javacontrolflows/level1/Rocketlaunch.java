e6bc77e83361b9cc5d9446e4a826f9c5ac85cfc4
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:42:10 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level1/Rocketlaunch.java
+++ javacontrolflows/level1/Rocketlaunch.java
@@ -4,20 +4,29 @@ Create a variable counter to take user inputted value for the countdown.
 Use the while loop to check if the counter is 1
 Inside a while loop, print the value of the counter and decrement the counter.
  */
-
-
 import java.util.Scanner;
 
 public class rocketlaunch {
+
     public static void main(String[] args) {
+
+        // Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
+
+        // Reading the starting number for the countdown
         int counter = sc.nextInt();
 
+        // while loop to perform countdown until counter reaches 1
         while (counter >= 1) {
+
+            // Printing the current countdown number
             System.out.println(counter);
+
+            // Decrementing the counter
             counter--;
         }
+
+        // Closing the scanner to release resources
         sc.close();
-        
     }
 }
