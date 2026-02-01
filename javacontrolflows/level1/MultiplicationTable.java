12c5432a2cc04e21b898169e3483c3e2e15cd30f
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:33:39 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level1/MultiplicationTable.java
+++ javacontrolflows/level1/MultiplicationTable.java
@@ -7,13 +7,23 @@ Using a for loop, find the multiplication table of number from 6 to 9 and print 
 import java.util.Scanner;
 
 public class MultiplicationTable {
+
     public static void main(String[] args) {
+
+        // Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
+
+        // Reading the number for which the multiplication table is needed
         int number = sc.nextInt();
 
+        // Loop to print the multiplication table from 6 to 9
         for (int i = 6; i <= 9; i++) {
+
+            // Printing the multiplication result for each value of i
             System.out.println(number + " * " + i + " = " + (number * i));
         }
+
+        // Closing the scanner to release resources
         sc.close();
     }
 }
