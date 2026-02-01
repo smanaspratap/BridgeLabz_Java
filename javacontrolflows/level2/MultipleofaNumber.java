0cd0e3fd72c146d76e02d6c6e5a20934df1b2d38
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:49:23 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level2/MultipleofaNumber.java
+++ javacontrolflows/level2/MultipleofaNumber.java
@@ -6,27 +6,33 @@ Inside the loop, check if i perfectly divide the number.
 If true, print the number and continue the loop.
  */
 
-
 import java.util.Scanner;
 
 class multipleofno {
+
     public static void main(String[] args) {
+
+        // Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
 
-        // Input
+        // Taking a number as input
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
 
+        // Displaying header message
         System.out.println("Multiples of " + number + " below 100:");
 
         // Loop backward from 100 to 1
         for (int i = 100; i >= 1; i--) {
+
+            // Checking if the current number is a multiple of the given number
             if (i % number == 0) {
                 System.out.println(i);
             }
         }
 
+        // Closing the scanner to release resources
         sc.close();
     }
-    
 }
+
