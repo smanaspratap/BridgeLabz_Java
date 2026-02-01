39e3a258f238bcb000a9348cf95a0df936cc52c9
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:29:30 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level1/CountdownFor.java
+++ javacontrolflows/level1/CountdownFor.java
@@ -4,13 +4,24 @@
 import java.util.Scanner;
 
 public class CountdownFor {
+
     public static void main(String[] args) {
+
+        // Scanner object to take input from the user
         Scanner sc = new Scanner(System.in);
+
+        // Reading the starting number for countdown
         int counter = sc.nextInt();
 
+        // for loop to print numbers from counter down to 1
         for (int i = counter; i >= 1; i--) {
+
+            // Printing the current value of i
             System.out.println(i);
         }
+
+        // Closing the scanner to release resources
         sc.close();
     }
 }
+
