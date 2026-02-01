5157bf8ad7fab60dbc105d8cb5f819be3600118f
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 01:14:13 +0530

    [Manas Pratap Singh]: Solved all the Java Methods Problems Level 1,2&3

--- javamethods/level3/StudentMarks.java
+++ javamethods/level3/StudentMarks.java
@@ -5,9 +5,10 @@ Take input for the number of students
 Write a method to generate random 2-digit scores for Physics, Chemistry, and Math (PCM) for the students and return the scores. This method returns a 2D array with PCM scores for all students
 Write a Method to calculate the total, average, and percentages for each student and return a 2D array with the corresponding values. Please ensure to round off the values to 2 Digits using the Math.round() method. 
 Finally, write a Method to display the scorecard of all students with their scores, total, average, and percentage in a tabular format using "\t".  */
-public class P12_StudentMarks {
+public class StudentMarks {
     public static void main(String[] args){
         int students=5;
+        //Computing the average
         for(int i=1;i<=students;i++){
             int p=(int)(Math.random()*100);
             int c=(int)(Math.random()*100);
@@ -15,6 +16,7 @@ public class P12_StudentMarks {
             int total=p+c+m;
             double avg=total/3.0;
             double per=(total/300.0)*100;
+            //printing the average
             System.out.println(i+"\t"+p+"\t"+c+"\t"+m+"\t"+total+"\t"+avg+"\t"+per);
         }
     }
