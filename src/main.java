201eab06acec46d1eb249f5a6dab2a9ab1c91cd0
smanaspratap <manaspratap2003@gmail.com>
2026-02-01 21:59:54 +0530

    [Manas Pratap Singh]: Welcome

--- src/main.java
+++ src/main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello and Welcome to my Repository!!");
+        System.out.println("Hello and Welcome");
     }
 }
