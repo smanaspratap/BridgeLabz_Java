2ce131e3595b36daaf248e899542d91ec7717ec6
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:51:10 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level2/Grades.java
+++ javacontrolflows/level2/Grades.java
@@ -6,51 +6,64 @@ Ensure the Output clearly shows the Average Mark as well as the Grade and Remark
 
 import java.util.Scanner;
 
-public class grades{
+public class grades {
+
     public static void main(String[] args) {
+
+        // Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
 
+        // Taking Physics marks as input
         System.out.print("Enter Physics marks: ");
         double physics = sc.nextDouble();
 
+        // Taking Chemistry marks as input
         System.out.print("Enter Chemistry marks: ");
         double chemistry = sc.nextDouble();
 
+        // Taking Maths marks as input
         System.out.print("Enter Maths marks: ");
         double maths = sc.nextDouble();
 
-        
+        // Calculating average marks
         double average = (physics + chemistry + maths) / 3;
 
+        // Variables to store grade and remarks
         char grade;
         String remarks;
 
-        // Grade calculation
+        // Determining grade and remarks based on average marks
         if (average >= 80) {
             grade = 'A';
             remarks = "Level 4, above agency-normalized standards";
-        } else if (average >= 70) {
+        }
+        else if (average >= 70) {
             grade = 'B';
             remarks = "Level 3, at agency-normalized standards";
-        } else if (average >= 60) {
+        }
+        else if (average >= 60) {
             grade = 'C';
             remarks = "Level 2, below but approaching standards";
-        } else if (average >= 50) {
+        }
+        else if (average >= 50) {
             grade = 'D';
             remarks = "Level 1, well below standards";
-        } else if (average >= 40) {
+        }
+        else if (average >= 40) {
             grade = 'E';
             remarks = "Level 1-, too below standards";
-        } else {
+        }
+        else {
             grade = 'R';
             remarks = "Remedial standards";
         }
 
-        // Output
+        // Displaying the results
         System.out.println("\nAverage Marks: " + average);
         System.out.println("Grade: " + grade);
         System.out.println("Remarks: " + remarks);
 
+        // Closing the scanner to release resources
         sc.close();
     }
 }
