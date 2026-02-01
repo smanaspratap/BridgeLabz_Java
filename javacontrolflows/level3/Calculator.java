d7a1995e46dbe1892ad551b25e838247e49aaf33
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:54:38 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level3/Calculator.java
+++ javacontrolflows/level3/Calculator.java
@@ -9,33 +9,54 @@ If op is +, perform addition between first and second; if it is -, perform subtr
 If op is neither of those 4 values, print Invalid Operator.
  */
 
-
 import java.util.Scanner;
 
 public class Calculator {
+
     public static void main(String[] args) {
+
+        // Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
 
+        // Reading the first number
         double first = sc.nextDouble();
+
+        // Reading the second number
         double second = sc.nextDouble();
+
+        // Reading the operator (+, -, *, /)
         String op = sc.next();
 
+        // Performing calculation based on the operator
         switch (op) {
+
             case "+":
+                // Addition
                 System.out.println(first + second);
                 break;
+
             case "-":
+                // Subtraction
                 System.out.println(first - second);
                 break;
+
             case "*":
+                // Multiplication
                 System.out.println(first * second);
                 break;
+
             case "/":
+                // Division
                 System.out.println(first / second);
                 break;
+
+            // If operator is not valid
             default:
                 System.out.println("Invalid Operator");
         }
+
+        // Closing the scanner to release resources
         sc.close();
     }
 }
+
