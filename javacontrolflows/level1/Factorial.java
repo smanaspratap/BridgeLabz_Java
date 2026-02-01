4a6ff780e85ec6e3c6455f0bb01be1708560af84
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:39:28 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level1/Factorial.java
+++ javacontrolflows/level1/Factorial.java
@@ -8,18 +8,40 @@ Print the factorial at the end.
 import java.util.Scanner;
 
 public class factorial {
+
     public static void main(String[] args) {
+
+        // Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
+
+        // Reading the number for which factorial is to be calculated
         int n = sc.nextInt();
-        if(n > 0){
+
+        // Checking if the number is positive
+        if (n > 0) {
+
+            // Variable to store the factorial result
             int fact = 1;
+
+            // Counter variable for the loop
             int i = 1;
-            while(i<= n){
-                fact = fact*i;
+
+            // Loop to calculate factorial using while loop
+            while (i <= n) {
+
+                // Multiply current value of fact by i
+                fact = fact * i;
+
+                // Move to the next number
                 i++;
             }
+
+            // Displaying the factorial result
             System.out.println(fact);
         }
+
+        // Closing the scanner to free resources
         sc.close();
     }
 }
+
