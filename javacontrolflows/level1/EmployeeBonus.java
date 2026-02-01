006d5ef85d123cd9c690fa0bdf6ae8df2e3962e2
Manas Pratap Singh <114471383+smanaspratap@users.noreply.github.com>
2026-02-02 00:30:17 +0530

    [Manas Pratap Singh]: Solved Level 1,2,3 Java ControlFlowsQuestions

--- javacontrolflows/level1/EmployeeBonus.java
+++ javacontrolflows/level1/EmployeeBonus.java
@@ -5,21 +5,36 @@ Take salary and year of service in the year as input.
 Print the bonus amount.
  */
 
-
 import java.util.Scanner;
 
 public class EmployeeBonus {
+
     public static void main(String[] args) {
+
+        // Scanner object to take input from the user
         Scanner sc = new Scanner(System.in);
+
+        // Reading the employee's salary
         double salary = sc.nextDouble();
+
+        // Reading the number of years of service
         int years = sc.nextInt();
 
+        // Checking if the employee is eligible for bonus
         if (years > 5) {
+
+            // Calculating 5% bonus on salary
             double bonus = salary * 0.05;
+
+            // Displaying the bonus amount
             System.out.println("Bonus Amount = " + bonus);
-        } else {
+        }
+        // If years of service is 5 or less
+        else {
             System.out.println("No Bonus");
         }
+
+        // Closing the scanner to release resources
         sc.close();
     }
 }
